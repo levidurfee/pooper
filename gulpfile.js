eb8cb5a94447dddc3c096553840780ee3e6b95f7
Levi Durfee <levi.durfee@gmail.com>
2017-07-01 18:24:21 -0400

    minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
+const minify = require('gulp-minify');
 
 gulp.task('transpile', function() {
   return gulp.src([
     './src/poop.js'
   ])
   .pipe(babel({ presets: ['es2015'] }))
+  .pipe(minify({
+    ext: {
+      min: '.min.js'
+    }
+  }))
   .pipe(gulp.dest('./public'));
 });
 
