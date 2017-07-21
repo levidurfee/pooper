f6fcfb7018c24c2e0b65a639afad60bed72e5b95
Levi Durfee <levi.durfee@gmail.com>
2017-07-21 11:38:20 -0400

    add gulp task for message js

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,24 @@ gulp.task('transpile', function() {
       min: '.min.js'
     }
   }))
-    .pipe(gulp.dest('./public/assets/js'));
+  .pipe(gulp.dest('./public/assets/js'));
+});
+
+gulp.task('transpile:message', function() {
+  return gulp.src([
+    './src/message.js'
+    ])
+  .pipe(babel({presets: ['es2015']}))
+  .pipe(minify({
+    ext: {
+      min: '.min.js'
+    }
+  }))
+  .pipe(gulp.dest('./public/assets/js'));
 });
 
 gulp.task('watch', function() {
   gulp.watch(['./src/poop.js'], ['transpile']);
+  gulp.watch(['./src/message.js'], ['transpile:message']);
 });
 
